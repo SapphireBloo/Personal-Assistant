51929458868278fe616ae61ef856e3ef61dc2ea4
SapphireBloo <156019660+SapphireBloo@users.noreply.github.com>
2025-06-24 11:27:08 -0500

    Renew

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  
+  base: "/Personal-Assistant/",
   plugins: [react(), ghPages()],
 });
